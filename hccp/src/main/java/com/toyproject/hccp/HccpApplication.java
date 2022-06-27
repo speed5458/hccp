0eb23b0937332aefb34b53109be72f421799d25a
최영준 <44596010+speed5458@users.noreply.github.com>
2022-06-27 20:43:11 +0900

    [HCCS] - Spring boot 웹사이트 구축 - 2 (기본 프로젝트 생성)

---
+++ hccp/src/main/java/com/toyproject/hccp/HccpApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.hccp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HccpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HccpApplication.class, args);
+	}
+
+}
---
+++ hccp/src/test/java/com/toyproject/hccp/HccpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.hccp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HccpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
